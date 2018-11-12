8d9eefd8737f15081ae403bda9053ec01c78d6cc
Tim <timothyhollabaugh@gmail.com>
2018-11-12 00:00:17 -0500

    Daily commit for Mon Nov 12 00:00:17 EST 2018

--- Datastructures/Projects/linked_list/node.h
+++ Datastructures/Projects/linked_list/node.h
@@ -38,4 +38,26 @@ namespace linked_list {
             node* link_field;
 
     };
+
+    size_t list_length(const node* head_ptr);
+
+    void list_head_insert(node*& head_ptr, const node::value_type& entry);
+
+    void list_insert(node* previous_ptr, const node::value_type& entry);
+
+    node* list_search(node* head_ptr, const node::value_type& target);
+
+    const node* list_search(const node* head_ptr, const node::value_type& target);
+
+    node* list_locate(node* head_ptr, size_t position);
+
+    const node* list_locate(const node* head_ptr, size_t position);
+
+    void list_head_remove(node*& head_ptr);
+
+    void list_remove(node* previous_ptr);
+
+    void list_clear(node*& head_ptr);
+
+    void list_copy(const node* source_ptr, node*& head_ptr, node*& tail_ptr);
 }
--- Datastructures/Projects/linked_list/sequence.cpp
+++ Datastructures/Projects/linked_list/sequence.cpp
@@ -10,6 +10,13 @@ namespace linked_list {
 
     sequence::sequence() {
         //fill your code here
+
+        // Initialize fields for an empty sequence
+        head_ptr = NULL;
+        tail_ptr = NULL;
+        cursor = NULL;
+        precursor = NULL;
+        many_nodes = 0;
     }
 
     sequence::sequence(const sequence& source)
@@ -23,14 +30,16 @@ namespace linked_list {
         if(source.cursor == NULL)
         {
             //fill your code here
-
+            cursor = NULL;
+            precursor = NULL;
         }
 
         // cursor equals to the head pointer
         else if(source.cursor == source.head_ptr)
         {
             //fill your code here
-
+            cursor = head_ptr;
+            precursor = NULL;
         }
 
         else
@@ -38,11 +47,30 @@ namespace linked_list {
             //fill your code here
             //hint: use list_locate() function to find the precursor
 
+            // Get the index of the node the source cursor is pointing to by iterating through the source
+            // nodes untill the current node is the same as the cursor node.
+            size_t cursor_index = 0;
 
+            for (
+                const node *current_node = source.head_ptr;
+                current_node != NULL;
+                current_node = current_node->link()
+            ) {
+                if (current_node == source.cursor) break;
+                cursor_index++;
+            }
+
+            // Find the node in the copied list that corresponds to the cursor in the source list
+            cursor = list_locate(head_ptr, cursor_index);
+
+            // Find the node for the precursor
+            precursor = list_locate(head_ptr, cursor_index-1);
         }
         //fill your code here
         // update the many_nodes variable
 
+        // Get the number of nodes in the list
+        many_nodes = list_length(head_ptr);
 
         return;
 
@@ -89,6 +117,10 @@ namespace linked_list {
         {
             //hint: list_head_insert()
 
+            // put the new node at the head of the list, and update the cursor and precursor
+            list_head_insert(head_ptr, entry);
+            cursor = head_ptr;
+            precursor = NULL;
         }
 
         else
@@ -97,6 +129,9 @@ namespace linked_list {
             // all other cases
             //hint: list_insert()
 
+            // Insert the new node after the precursor, and update the cursor
+            list_insert(precursor, entry);
+            cursor = precursor->link();
         }
         //update the variables
         many_nodes = many_nodes + 1;
@@ -166,35 +201,52 @@ namespace linked_list {
     {
         if(this == &source){
             //fill your code here
+            // The two lists are the same list, no need to do anything
+            return;
         }
 
+        // Copy the source list to this list
+        list_copy(source.head_ptr, head_ptr, tail_ptr);
 
-        //fill your code here
-        // copy the linked list
-        //hint: use list_copy() function
-
-        if(source.cursor ==  NULL)
+        // There is no cursor in the source list
+        if(source.cursor == NULL)
         {
-            //fill your code here
-
+            cursor = NULL;
+            precursor = NULL;
         }
 
-
-        //copy the cursor and precursor
+        // cursor equals to the head pointer
         else if(source.cursor == source.head_ptr)
         {
-
-            //fill your code here
+            cursor = head_ptr;
+            precursor = NULL;
         }
 
         else
         {
-             //fill your code here
-            //hint: use list_locate() function to find the precursor
-        }
+            // Get the index of the node the source cursor is pointing to by iterating through the source
+            // nodes untill the current node is the same as the cursor node.
+            size_t cursor_index = 0;
 
+            for (
+                const node *current_node = source.head_ptr;
+                current_node != NULL;
+                current_node = current_node->link()
+            ) {
+                if (current_node == source.cursor) break;
+                cursor_index++;
+            }
+
+            // Find the node in the copied list that corresponds to the cursor in the source list
+            cursor = list_locate(head_ptr, cursor_index);
+
+            // Find the node for the precursor
+            precursor = list_locate(head_ptr, cursor_index-1);
+        }
+        // Get the number of nodes in the list
         many_nodes = list_length(head_ptr);
-        return ;
+
+        return;
     }
 
     sequence::value_type sequence::current() const {
